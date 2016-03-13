3d0f6a710cae26c0668d8d1669216cf40dd30d16
Udayan Shevade <udayan.shevade@gmail.com>
2016-03-12 19:23:44 -0800

    Add same-address checkbox, track address input updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,108 @@
-(function(win) {
+var app = {};
+app = (function(win) {
 
-  var app = {};
   var doc = win.document;
 
-  app.View = function(model) {
+  app.Model = function() {
+    var that = this;
+
+    that.shipping = {
+      'shipping-name': '',
+      'shipping-address-line1': '',
+      'shipping-address-line2': '',
+      'shipping-city': '',
+      'shipping-state': '',
+      'shipping-postal-code': '',
+      'shipping-country': ''
+    };
+
+    that.billing = {
+      'billing-name': '',
+      'billing-address-line1': '',
+      'billing-address-line2': '',
+      'billing-city': '',
+      'billing-state': '',
+      'billing-postal-code': '',
+      'billing-country': ''
+    };
+
+    that.cloneShipping = function(prefix) {
+      var splitName, key;
+      var shipping =  app.model.shipping,
+        clonedObj = {};
+      for (var prop in shipping) {
+        splitName = prop.split('-');
+        splitName.splice(0, 1, prefix);
+        key = splitName.join('-');
+        clonedObj[key] = shipping[prop];
+      }
+      return clonedObj;
+    };
+
+  };
+
+  app.Controller = function() {
+
+    var that = this;
+
+    that.init = function() {
+      app.model = new app.Model(that);
+      app.view = new app.View(that);
+    };
+
+    that.applySameAddress = function() {
+      app.view.getInfo(app.model.shipping);
+      app.model.billing = app.model.cloneShipping('billing');
+      app.view.setInfo(app.model.billing);
+    };
+
+    that.updateInput = function(e) {
+      var el = e.srcElement;
+      var id = el.id;
+      var type = id.split('-')[0];
+      app.model[type][id] = el.value;
+      if (type === 'shipping' &&
+        app.view.addressCheckbox.checked) {
+        that.applySameAddress();
+      }
+    };
+
+    that.init();
+
+  };
+
+  app.View = function(controller) {
 
     var that = this;
 
     this.init = function() {
+      that.initNav();
       that.initTabs();
+      that.initAddressCheckbox();
+      that.watchAddress('shipping');
+      that.watchAddress('billing');
+    };
+
+    that.initNav = function() {
+      that.navList = doc.getElementsByClassName('sections-nav__li');
+      that.sectionNav = doc.getElementsByClassName('sections-nav__list')[0];
+      var nav, navId;
+      that.sectionNav.addEventListener('mouseover', function(e) {
+        if (e.target && e.target.nodeName == 'LI') {
+          navId = e.srcElement.id.split('-')[0];
+          that.showActiveTab(navId);
+          that.showActiveNav(navId);
+        }
+      });
+      that.showActiveNav('ship');
     };
 
     that.initTabs = function() {
-      that.sectionNav = doc.getElementsByClassName('sections-nav__li');
-      that.shipNav = doc.getElementById('ship-nav');
-      that.billNav = doc.getElementById('bill-nav');
-      that.confirmNav = doc.getElementById('confirm-nav');
-      that.navList = [that.shipNav, that.billNav, that.confirmNav];
-
-      that.shipTab = doc.getElementById('ship-tab');
-      that.billTab = doc.getElementById('bill-tab');
-      that.confirmTab = doc.getElementById('confirm-tab');
-      that.tabList = [that.shipTab, that.billTab, that.confirmTab];
-
-      var nav, navId;
-      for (var i = 0; i < 3; i++) {
-        nav = that.sectionNav[i];
-        nav.addEventListener('click', function(e) {
-          navId = e.srcElement.id.split('-')[0];
-          that.showActiveTabs(navId);
-        });
-      }
-      that.showActiveTabs('ship');
+      that.tabList = doc.getElementsByClassName('form-division');
+      that.showActiveTab('ship');
     };
 
-    that.showActiveTabs = function(id) {
+    that.showActiveTab = function(id) {
       var tab, tabId;
       var thisId = id + '-tab';
       for (var i = 0; i < that.tabList.length; i++) {
@@ -44,11 +112,57 @@
       }
     };
 
+    that.showActiveNav = function(id) {
+      var nav, navId;
+      var thisId = id + '-nav';
+      for (var i = 0; i < that.navList.length; i++) {
+        nav = that.navList[i];
+        navId = nav.id;
+        nav.classList.toggle('sections-nav__li--selected', (navId === thisId));
+      }
+    };
+
+    that.initAddressCheckbox = function() {
+      that.addressCheckbox = doc.getElementById('same-address-checkbox');
+      that.addressCheckbox.addEventListener('change', function(e) {
+        if (this.checked) {
+          app.controller.applySameAddress();
+        }
+      });
+    };
+
+    that.setInfo = function(obj) {
+      var detailEl;
+      for (var detail in obj) {
+        detailEl = doc.getElementById(detail);
+        detailEl.value = obj[detail] || '';
+      }
+    };
+
+    that.getInfo = function(obj) {
+      var detailEl;
+      for (var detail in obj) {
+        detailEl = doc.getElementById(detail);
+        obj[detail] = detailEl.value || '';
+      }
+    };
+
+    that.watchAddress = function(type) {
+      var section = doc.getElementById(type);
+      var inputs = section.getElementsByClassName('form-entry__input');
+      var i, inp, len;
+      for (i = 0, len = inputs.length; i < len; i++) {
+        inp = inputs[i];
+        inp.onchange = controller.updateInput;
+      }
+    };
+
     this.init();
 
   };
 
-  app.View();
+  app.controller = new app.Controller();
+
 
   return app;
 
